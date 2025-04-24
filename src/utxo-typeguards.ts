b1497b84a6eedaeee8cc083bd1d0f5cd38d55fe4
naward-cs <nicholas.ward.cyber@gmail.com>
2025-04-24 14:45:10 -0500

    changed name of utxo-types to utxo-typeguards to make more since updated ecurve to latest version and added @types for typescript support

--- src/utxo-typeguards.ts
+++ src/utxo-typeguards.ts
@@ -1,29 +1,26 @@
-;
+import type {Network} from './types'
 
-// var typeforce = require('typeforce')
+import {bigi as BigInteger} from 'bigi'
+import {Point} from 'ecurve'
+import {
+  isBuffer,
+  isString,
+  isUInt16,
+  isUInt32,
+  isUInt53,
+  isUInt8,
+  oneOf,
+} from './lib/type-guard'
 
-import {isString, isUInt53} from './lib/type-guard'
+import ECSignature = require('./ecsignature')
 
-
-
-
-
-;
-
-
-
-
-
-
-
-
-//Typeguard lib numbers.ts
+//Typeguard already exists in numbers.ts
 // var UINT31_MAX = Math.pow(2, 31) - 1
 // function UInt31 (value) {
 //   return typeforce.UInt32(value) && value <= UINT31_MAX
 // }
 
-export function BIP32Path(value: unknown):boolean {
+export function BIP32Path(value: unknown): boolean {
   return isString(value) && !!value.match(/^(m\/)?(\d+'?\/)*\d+'?$/)
 }
 BIP32Path.toJSON = function () {
@@ -39,24 +36,38 @@ export function isSatoshi(value: unknown): boolean {
   return isUInt53(value) && <number>value <= SATOSHI_MAX
 }
 
+function isNewtorkVersion(value: unknown): value is number {
+  return oneOf([isUInt8, isUInt16], value)
+}
+
+export function isNetwork(value: Network): value is Network {
+  return (
+    oneOf([isBuffer, isString], value.messagePrefix) &&
+    isUInt32(value.bip32.public) &&
+    isUInt32(value.bip32.private) &&
+    isNewtorkVersion(value.pubKeyHash) &&
+    isNewtorkVersion(value.scriptHash) &&
+    isUInt8(value.wif)
+  )
+}
 // external dependent types
-//Todo
+// replacement for
 // const BigInt = typeforce.quacksLike('BigInteger')
+export function isBigiInt(value: unknown): value is BigInteger {
+  return value instanceof BigInteger
+}
 // const ECPoint = typeforce.quacksLike('Point')
+export function isECPoint(value:unknown):value is Point{
+  return value instanceof Point
+}
 //Todo
 // exposed, external API
+// replacement for ECSignature typeforce to make it more accurate
 // const ECSignature = typeforce.compile({ r: BigInt, s: BigInt })
-// const networkVersion = typeforce.oneOf(typeforce.UInt8, typeforce.UInt16)
-// const Network = typeforce.compile({
-  // messagePrefix: typeforce.oneOf(typeforce.Buffer, typeforce.String),
-  // bip32: {
-    // public: typeforce.UInt32,
-    // private: typeforce.UInt32
-  // },
-  // pubKeyHash: networkVersion,
-  // scriptHash: networkVersion,
-  // wif: typeforce.UInt8
-// })
+export function isECSignature(value: unknown): value is ECSignature {
+  return value instanceof ECSignature
+}
+
 
 
 // export function isDefinedHashType(hashType: number):hashType is number {
@@ -89,7 +100,7 @@ export function isSatoshi(value: unknown): boolean {
 // }
 
 // export function asMinimalOP(buffer: Buffer) {
-  
+
 //   if (buffer.length === 0) return OPS.OP_0
 //   if (buffer.length !== 1) return
 //   if (buffer[0] >= 1 && buffer[0] <= 16) return OP_INT_BASE + buffer[0]
